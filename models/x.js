c9cfbd044ebbccba35afb301b7f3bde5e5f0e2d7
Soraya-j <21255@ecam.be>
2023-12-14 15:19:29 +0100

    add folder

---
+++ models/x.js
@@ -0,0 +1,7 @@
+import Model from './Model.js';
+
+export default class [table] extends Model {
+
+  static table = "database.table";
+  static primary = ["id"];
+}
--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 import express from 'express';
+import [table] from './models/x.js';
 
 const app = express();
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(4000);
+// app.get('/', async function(req,res){
+//     const wishlist = await tele.loadMany({Achete : 0})
+//     const have = await tele.loadMany({Achete : 1})
+//     let sum = 0;
+//     for(let i = 0;i<have.length ;i++){
+//         sum = sum + have[i].Prix;
+//     };
+//     res.render('TvList.ejs', {wishlist,have,sum});
+// });
+
+// app.post("/add", async function (req, res) {
+//   const tv = new tele();
+//   tv.Marque = req.body.Marque;
+//   tv.Prix = parseInt(req.body.Prix);
+//   tv.Taille = parseInt(req.body.Taille);
+//   tv.Achete = 0;
+//   await tv.save();
+//   res.redirect('/');
+// });
+
+// app.post("/buy", async function (req, res) {
+//     const tv = await tele.load({id : req.body.idtv})
+//     tv.update({Achete : 1});
+//     tv.update({Casse : 0})
+//     await tv.save();
+//     res.redirect('/');
+// });
+
+// app.post("/broke", async function (req, res) {
+//     const tv = await tele.load({id : req.body.idtv})
+//     tv.update({Casse : 1});
+//     tv.update({Cause : req.body.Cause})
+//     await tv.save();
+//     res.redirect('/');
+// });
+
+app.listen(80);
